4f8217774d255f9c1fc53252326097a3721cce01
kwaxite <146014461+kwaxite@users.noreply.github.com>
2023-11-24 09:34:54 +0000

    dom methods

---
+++ dom2/dom.js
@@ -0,0 +1,7 @@
+const container = document.getElementById('container')
+
+
+const controls = document.getElementsByClassName('controls')
+
+const display = document.querySelector('.display')
+console.log(display.nextElementSibling) 
