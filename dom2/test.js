365bf875b5e5dcb334459d6a7c3bf7092b5d2e5f
kwaxite <146014461+kwaxite@users.noreply.github.com>
2023-11-27 20:33:16 +0000

    update with new events
    included several test examples of DOM event handling

---
+++ dom2/test.js
@@ -0,0 +1,33 @@
+const divOne = document.querySelector('.display');
+
+
+// // divOne.classList.add("controls")
+
+// const divThree = document.createElement('div')
+
+// const container = document.querySelector('#container')
+
+// container.append(divThree)
+
+// divThree.setAttribute('class', 'newDiv')
+
+// console.log(container)
+
+
+// divThree.textContent = 'I am a new Div'
+// divThree.style.cssText = "background-color:pink;font-size:5rem;border:2px dashed green;color:white;"
+// // divThree.style.fontSize = '2rem'
+
+
+// divOne.style.cssText = "border:3px solid green; background-color: yellow";
+// console.log(divOne)
+
+button  = document.querySelector('.btn')
+console.log(button)
+
+function change(){
+    let info = document.querySelector('#myDIV');
+    info.classList.toggle('mystyle')
+}
+
+button 
